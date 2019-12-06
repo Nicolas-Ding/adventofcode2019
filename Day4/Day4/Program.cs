24dfc1970cb0454a279bd901b5b8c678d4393de0
RyZum <ryzumm@gmail.com>
2019-12-06 08:57:07 +0100

    Initial commit with Day 4 - 6

---
+++ Day4/Day4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int correct = 0;
+            for (int i = 246540; i <= 787419; i++)
+            {
+                correct += IsValid(i) ? 1 : 0;
+            }
+            Console.WriteLine(correct);
+
+            //Console.WriteLine(IsValid(112444));
+        }
+
+        public static bool IsValid(int s)
+        {
+            int lastDigit = 10;
+            bool hasAdjacentDuplicates = false;
+            int minCountOfAdjacent = 10;
+            int countOfAdjacent = 0;
+            while (s > 0)
+            {
+                int digit = s % 10;
+                if (digit == lastDigit)
+                {
+                    hasAdjacentDuplicates = true;
+                    countOfAdjacent++;
+                }
+                else
+                {
+                    if (countOfAdjacent > 0 && countOfAdjacent < minCountOfAdjacent)
+                    {
+                        minCountOfAdjacent = countOfAdjacent;
+                    }
+                    countOfAdjacent = 0;
+                }
+                // We are breaking the increasing rule
+                if (digit > lastDigit)
+                {
+                    return false;
+                }
+                lastDigit = digit;
+                s = s / 10;
+            }
+            if (countOfAdjacent > 0 && countOfAdjacent < minCountOfAdjacent)
+            {
+                minCountOfAdjacent = countOfAdjacent;
+            }
+            //Console.WriteLine(minCountOfAdjacent);
+            return hasAdjacentDuplicates && minCountOfAdjacent == 1;
+        }
+    }
+}
---
+++ Day6/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            Dictionary<string, List<string>> edges = new Dictionary<string, List<string>>();
+            line = Console.ReadLine();
+            while (!string.IsNullOrEmpty(line))
+            {
+                string[] planets = line.Split(')');
+                if (!edges.ContainsKey(planets[0]))
+                {
+                    edges[planets[0]] = new List<string>();
+                }
+                edges[planets[0]].Add(planets[1]);
+                if (!edges.ContainsKey(planets[1]))
+                {
+                    edges[planets[1]] = new List<string>();
+                }
+                edges[planets[1]].Add(planets[0]);
+                line = Console.ReadLine();
+            }
+
+            List<string> current = new List<string> {"YOU"};
+            int step = 0;
+            while (!current.Contains("SAN"))
+            {
+                List<string> nextCurrent = new List<string>();
+                foreach (string planet in current)
+                {
+                    if (edges.ContainsKey(planet))
+                    {
+                        nextCurrent.AddRange(edges[planet]);
+                        edges.Remove(planet);
+                    }
+                }
+
+                current = nextCurrent;
+                step++;
+            }
+            Console.WriteLine(step);
+        }
+    }
+}
