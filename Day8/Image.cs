cd156d2a76618ef34b6392b8e02d618288206453
RyZum <ryzumm@gmail.com>
2019-12-08 09:22:32 +0100

    Day 8 Part 2

--- Day8/Image.cs
+++ Day8/Image.cs
@@ -22,5 +22,32 @@ namespace Day8
                 Layers.Add(image.Substring(i, Width * Height));
             }
         }
+        
+        public string GetFinalLayer()
+        {
+            StringBuilder res = new StringBuilder();
+            for (int i = 0; i < Width * Height; i++)
+            {
+                int layerToadd = 0;
+                char charToAdd = Layers[layerToadd][i];
+                while (charToAdd == '2')
+                {
+                    layerToadd++;
+                    charToAdd = Layers[layerToadd][i];
+                }
+                res.Append(charToAdd);
+            }
+
+            return res.ToString();
+        }
+
+        public void PrintFinalLayer()
+        {
+            string output = GetFinalLayer();
+            for (int i = 0; i < output.Length; i += Width)
+            {
+                Console.WriteLine(output.Substring(i, Width));
+            }
+        }
     }
 }
