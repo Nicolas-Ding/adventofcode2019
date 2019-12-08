131b0f429e5927eb122ad4f244a639a5a0c896b3
RyZum <ryzumm@gmail.com>
2019-12-08 09:12:32 +0100

    Day 8 Part 1

---
+++ Day8/Image.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day8
+{
+    public class Image
+    {
+        int Width { get; }
+
+        int Height { get; }
+
+        public List<string> Layers { get; } = new List<string>();
+
+        public Image(int width, int height, string image)
+        {
+            Width = width;
+            Height = height;
+
+            for (int i = 0; i < image.Length; i += width * height)
+            {
+                Layers.Add(image.Substring(i, Width * Height));
+            }
+        }
+    }
+}
