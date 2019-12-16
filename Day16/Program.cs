d114866404cc1794422fdeb8abd1914242becf5c
Nicolas Ding <niding@microsoft.com>
2019-12-16 12:55:54 +0100

    A little bit more optimisation

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -18,6 +18,7 @@ namespace Day16
             string input = "59767332893712499303507927392492799842280949032647447943708128134759829623432979665638627748828769901459920331809324277257783559980682773005090812015194705678044494427656694450683470894204458322512685463108677297931475224644120088044241514984501801055776621459006306355191173838028818541852472766531691447716699929369254367590657434009446852446382913299030985023252085192396763168288943696868044543275244584834495762182333696287306000879305760028716584659188511036134905935090284404044065551054821920696749822628998776535580685208350672371545812292776910208462128008216282210434666822690603370151291219895209312686939242854295497457769408869210686246";
             input = "19617804207202209144916044189917";
             input = "03036732577212944063491565474664";
+            input = "03036732577212944063491565474664";
 
             for (int i = 0; i < 10000; i++)
             {
@@ -26,7 +27,7 @@ namespace Day16
 
             Line = builder.ToString();
 
-            _factorList = new List<int>() {0, 1, 0, -1};
+            _factorList = new List<int> {0, 1, 0, -1};
 
             for (int p = 0; p < 100; p++)
             {
@@ -38,24 +39,34 @@ namespace Day16
                 }
 
                 Line = result;
+                //Console.WriteLine(Line);
             }
-            //Console.WriteLine(Line);
+            Console.WriteLine(Line);
             Console.WriteLine(Line.Substring(Int32.Parse(Line.Substring(0, 7)), 8));
         }
 
-        public static int ComputeDigits(int digit) // should be 0 at start
+        public static int ComputeDigits(int line) // should be 0 at start
         {
+            int digit = line + 1;
             int result = 0;
-            IEnumerator<int> multiplicators = GetMultiplicators(digit + 1).GetEnumerator();
-            multiplicators.MoveNext();
 
-            for(int i = digit; i < Line.Length; i++)
+            int c = 1;
+
+            while (c * digit - 1 < Line.Length)
             {
-                result += (int) char.GetNumericValue(Line[i]) * multiplicators.Current;
-                multiplicators.MoveNext();
+                for (int i = c * digit - 1; i < Math.Min(Line.Length, (c + 1) * digit - 1); i++)
+                {
+                    result += (int) char.GetNumericValue(Line[i]);
+                }
+
+                for (int i = (c + 2) * digit - 1; i < Math.Min(Line.Length, (c + 3) * digit - 1); i++)
+                {
+                    result -= (int) char.GetNumericValue(Line[i]);
+                }
+
+                c+=4;
             }
 
-            multiplicators.Dispose();
             return result;
         }
 
