6bdf458865b3aab20a47951e1adbcf3984d756a1
RyZum <ryzumm@gmail.com>
2019-12-20 09:35:18 +0100

    Day 20 WIP

---
+++ Day20/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<(int, int), char> map = new Dictionary<(int, int), char>();
+            System.IO.StreamReader file = new System.IO.StreamReader(@"input.txt");
+            string line;
+            int i = 0;
+            while ((line = file.ReadLine()) != null)
+            {
+                for (int j = 0; j < line.Length; j++)
+                {
+                    map[(i,j)] = line[j];
+                }
+                i++;
+            }
+        }
+    }
+}
